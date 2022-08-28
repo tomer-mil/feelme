eea349362e9c4e22c60016c9e0f318594e98b712
shellyrazinsky <shellyrazinsky@gmail.com>
2022-08-28 20:01:04 +0300

    - Re-designed create_csv.py and put the existing code into two functions. - Started working on getting top genres, no success as for now.

--- authorization.py
+++ authorization.py
@@ -6,6 +6,7 @@ load_dotenv()
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
+
 def authorize():
     app_token = tk.request_client_token(CLIENT_ID, CLIENT_SECRET)
     return tk.Spotify(app_token)
--- create_csv.py
+++ create_csv.py
@@ -13,40 +13,43 @@ data_dict = {"id": [], "genre": [], "track_name": [], "artist_name": [],
              "valence": [], "energy": []}
 
 
-for g in tqdm(genres):
-    recs = sp.recommendations(genres=[g], limit=N_RECS)
-    recs = eval(
-        recs.json().replace("null", "-999").replace("false", "False").replace("true", "True"))["tracks"]
+def create_rec():
+    for g in tqdm(genres):
+        recs = sp.recommendations(genres=[g], limit=N_RECS)
+        recs = eval(
+            recs.json().replace("null", "-999").replace("false", "False").replace("true", "True"))["tracks"]  # TODO: change replace to dumps, this is bad practice
 
-    for track in recs:
+        for track in recs:
 
-        # ID and Genre
-        data_dict["id"].append(track["id"])
-        data_dict["genre"].append(g)
+            # ID and Genre
+            data_dict["id"].append(track["id"])
+            data_dict["genre"].append(g)
 
-        # Metadata
-        track_meta = sp.track(track["id"])
-        data_dict["track_name"].append(track_meta.name)
-        data_dict["artist_name"].append(track_meta.album.artists[0].name)
+            # Metadata
+            track_meta = sp.track(track["id"])
+            data_dict["track_name"].append(track_meta.name)
+            data_dict["artist_name"].append(track_meta.album.artists[0].name)
 
-        # Valence and energy
-        track_features = sp.track_audio_features(track["id"])
-        data_dict["valence"].append(track_features.valence)
-        data_dict["energy"].append(track_features.energy)
+            # Valence and energy
+            track_features = sp.track_audio_features(track["id"])
+            data_dict["valence"].append(track_features.valence)
+            data_dict["energy"].append(track_features.energy)
 
-        # Wait 0.2 seconds per track so that the api doesnt overheat
-        time.sleep(0.2)
+            # Wait 0.2 seconds per track so that the api doesnt overheat
+            time.sleep(0.2)
 
 ##################
 ## PROCESS DATA ##
 ##################
 
-# Store data in dataframe
-df = pd.DataFrame(data_dict)
 
-# Drop duplicates
-df.drop_duplicates(subset="id", keep="first", inplace=True)
-df.to_csv("valence_arousal_dataset_tomer2.csv", index=False)
+def rec_dict_to_df(data_dict):
+    # Store data in dataframe
+    df = pd.DataFrame(data_dict)
 
-# Create mood vector and add it to the existing dataframe
-df["mood_vec"] = df["valence", "energy"].values.tolist()
+    # Drop duplicates
+    df.drop_duplicates(subset="id", keep="first", inplace=True)
+    df.to_csv("valence_arousal_dataset_tomer2.csv", index=False)
+
+    # Create mood vector and add it to the existing dataframe
+    df["mood_vec"] = df["valence", "energy"].values.tolist()
