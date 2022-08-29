31ec8568afa1a64e17e1b0f6905b6a27cb5f1f78
shellyrazinsky <shellyrazinsky@gmail.com>
2022-08-29 14:53:58 +0300

    Tried to get user's token, in order to get its top artists

--- authorization.py
+++ authorization.py
@@ -3,8 +3,21 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+
+
+# conf = tk.config_from_file("/Users/shellyrazinsky/PycharmProjects/feelme/.env")
+# conf = (client_id, client_secret, redirect_uri)
+# TODO: replace config_from_file to config_from_environment to get user's data (local)
+# print(*conf)
+
+# scope = tk.scope.user_top_read
+# token = tk.prompt_for_user_token(*conf, 'user-top-read')
+
+spotify = tk.Spotify('AQBl5KOuGAP-Trni7STXf53dLOKvIIiF56HKFK5YxyZNAXwDk_EnT5TKJkXVKHgwKpoRZ9RMS0VUp46rj4Ic99V39uUTKRzO0ST6Uu6C9azAUI8UIyv3ygboTgpsFOeuFzg')
+artists = spotify.current_user_top_artists(limit=10)
+print(artists)
 
 
 def authorize():
--- get_top_genres.py
+++ get_top_genres.py
@@ -1,19 +1,6 @@
-from create_csv import sp
-import tekore as tk
-from authorization import CLIENT_ID, CLIENT_SECRET
-
-
-# scope = tk.scope.user_top_read
-# token = tk.prompt_for_user_token(sp, scope=scope)
-# sp.playback_currently_playing()
-# top_artists = sp.current_user_top_artists()
-# print(top_artists)
-
 import tekore as tk
 
-redirect_uri = 'http://localhost/'
-conf = (CLIENT_ID, CLIENT_SECRET, redirect_uri)
-token = tk.prompt_for_user_token(*conf, scope=tk.scope.every)
+client_id, client_secret, redirect_uri = tk.config_from_environment()
 
-spotify = tk.Spotify(token)
-tracks = spotify.current_user_top_tracks(limit=10)
+conf = tk.config_from_environment(return_refresh=True)
+client_id, client_secret, redirect_uri, user_refresh = conf
